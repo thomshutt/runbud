de9f1542fa2c52509edb2029cee10af4c6f2d955
Thom Shutt <thom@thomshutt.com>
2015-01-25 13:27:25 +0000

    More playing around with migrations

--- regenerateMigrations.sh
+++ regenerateMigrations.sh
@@ -1,1 +1,6 @@
 java -jar target/runbud-1.0-SNAPSHOT.jar db dump config-local.yml > src/main/resources/migrations.xml
+sed 's/BIGINT(19)/BIGINT/' src/main/resources/migrations.xml > src/main/resources/migrations2.xml
+sed 's/BOOLEAN(1)/BOOLEAN/' src/main/resources/migrations2.xml > src/main/resources/migrations.xml
+sed 's/DOUBLE(17)/NUMERIC(10,10)/' src/main/resources/migrations.xml > src/main/resources/migrations2.xml
+# rm src/main/resources/migrations2.xml
+mv src/main/resources/migrations2.xml src/main/resources/migrations.xml
